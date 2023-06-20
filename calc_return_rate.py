cd9f63cdc9ba6d8d09fb3c8c9a0f9e4f4c2f95d4
m-henderson <maddiehenderson12@gmail.com>
2023-06-20 16:46:21 +0100

    just graphing

--- calc_return_rate.py
+++ calc_return_rate.py
@@ -16,18 +16,24 @@ with open ('precip_pixels_sample.pkl', 'rb') as file:
 with open ('ndvi_sample_res.pkl', 'rb') as file:
     ndvi_res, ndvi_meta, ndvi_bound = pickle.load(file)
 
+date_list = pd.date_range(start='5/1/2013', periods=120, freq='MS')
 
-pix =  ndvi_res.iloc[27].filter(like='20') #get one sample
-precip = precip_df.iloc[27].filter(like='20')
-fig, host = plt.subplots()
-ax2 = host.twinx()
-host.set_xlabel('date')
-host.set_ylabel('NDVI')
-ax2.set_ylabel('monthly precip mm/mo')
-p1 = host.plot(pix, 'r-', label='ndvi')
-p2 = host.plot(precip, 'b*', label='monthly precip mm')
-host.legend()
-# plt.xticks(rotation=90)
-host.autofmt_xdate()
+pix =  ndvi_res.iloc[20].filter(like='20') #get one sample from a pixel
+precip = precip_df.iloc[20].filter(like='20')
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, sharey=False)
+# ax2 = host.twinx()
+ax1.set_xlabel('month')
+ax1.set_ylabel('monthly precip mm/mo' )
+ax1.set_ylim(0, 600)
+ax1.bar(x=date_list, height=precip, width=10, label='monthly precip mm')
+
+ax2.set_ylabel('NDVI residual')
+ax2.set_ylim(-0.25, 0.25)
+ax2.plot(date_list, pix, 'r*', label='ndvi res')
+fig.legend()
+ax1.grid(axis='x', which='major')
+ax2.grid(axis='x', which='major')
+fig.autofmt_xdate(rotation=45)
 plt.show()
+
 print('done')
