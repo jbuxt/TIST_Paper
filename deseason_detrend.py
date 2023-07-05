71dd8c2ae6c8de40bfc402a30af86d19c38c1ed0
m-henderson <maddiehenderson12@gmail.com>
2023-07-05 14:35:14 +0100

    WIP on moving away from the ndvi reconstruction

--- deseason_detrend.py
+++ deseason_detrend.py
@@ -19,22 +19,59 @@ from sav_golay import savitzky_golay_filtering as sgf
 #     ndvi_df, ndvi_meta, ndvi_bound = pickle.load(file)
 
 ndvi_df = pd.read_csv('ndvi_pixels_Theraka.csv', nrows=100) #TEMP for testing 
-
+dates = pd.date_range(start='5/1/2013', periods=120, freq='MS')
 #apply smoothing and filling function (Chen 2004) to each timeseries (rows, axis =1)
 #this takes a minute
 #would be nice to vectorize this but not sure that's possible
-smoothed_ndvi_df = ndvi_df.filter(like='20', axis =1).astype("float32").apply(sgf, axis = 1, result_type='broadcast') #
-smoothed_ndvi_df [['row','col','tist', 'county']]=ndvi_df[['row','col','tist', 'county']]
+#right now limit to fill is 3 because a season is 3 months so dont want to miss 
+#any holes larger than 3 months are not filled and left as NaN
+#And only the longest three continuous sections are kept and smoothed after filling 
 
+'''
+smoothed_ndvi_df = ndvi_df.filter(like='20', axis =1).astype("float32").apply(sgf, axis = 1, result_type='broadcast') #
+'''
+
+# What if we just linearly interpolate, don't think i have enough resolution 
+# to really bother with this thing
+
+smoothed_ndvi_df = ndvi_df.filter(like='20', axis =1).astype("float32").interpolate(method='linear', limit=3, axis = 1, limit_area='inside')
+smoothed_ndvi_df[['row','col','tist', 'county']]=ndvi_df[['row','col','tist', 'county']]
 # del ndvi_df # save space 
 
-## consider: the wrapping? not sure why this is skipping some bits entirely 
+final_ts = interp_ts.copy()
+final_ts.iloc[:]=np.nan
+ 
+# are there any holes still missing? If so,
+# get any portions of data longer than 48 pts (should be only 1 or 2 sections since only 120 pts)
+# Extract out relevant column from dataframe as array
+m = np.concatenate(( [True], np.isnan(interp_ts.values), [True] ))  # Mask
+#kinda assuming there are no completely empty pixels 
+#https://stackoverflow.com/questions/41494444/pandas-find-longest-stretch-without-nan-values
+
+ss = np.flatnonzero(m[1:] != m[:-1]).reshape(-1,2)   # Start-stop limits
+intervals, dum = np.shape(ss) #how many intervals are there 
+#get any sections longer than 48 pts 
+ss = ss[((ss[:,1] - ss[:,0]) >= 48)] #check this 
+#set everything outside of these two intervals to NaN
+mask = 
+# if intervals > 2: #if more than 3 pick the top 3
+#     # ss = ss[np.argpartition((ss[:,1] - ss[:,0]), -3)[-3:]]
+#     ss = ss[((ss[:,1] - ss[:,0]) >= 48)]
+#     intervals = 3
+
+# start,stop = ss[(ss[:,1] - ss[:,0]).argmax()]  # Get max interval, interval limits
+for x in range(0,intervals):
+    start, stop = ss[x, :] #get idx of the interval 
+    temp_ts = interp_ts.iloc[start:stop]
+    #assign the smoothed section to the output series then repeat for other intervals
+    final_ts.iloc[start:stop] = smooth_ts
+
 
 for i in range(1,5):
     plt.subplot(4, 1, i)
-    plt.plot(smoothed_ndvi_df.columns, smoothed_ndvi_df.iloc[i], 'bo', smoothed_ndvi_df.columns, 
-           ndvi_df.filter(like='20', axis =1).iloc[i], 'g*')
-    plt.title('pixel '+str(i))
+    plt.plot(dates, smoothed_ndvi_df.filter(like='20').iloc[i], 'bo', dates, 
+           ndvi_df.filter(like='20').iloc[i], 'g*')
+    # plt.title('pixel '+str(i))
 plt.suptitle('Sample NDVI pixels with Reconstruction and Smoothing')
 plt.show()
 
--- sav_golay.py
+++ sav_golay.py
@@ -1,6 +1,4 @@
 # From 
-# https://gis.stackexchange.com/questions/173721/ ...
-# reconstructing-modis-time-series-applying-savitzky-golay-filter-with-python-nump/173747#173747
 # Applies the method from Chen et al 2004 for NDVI time series reconstruction
 # slightly modified for my purposes
 
@@ -8,67 +6,101 @@ import pandas as pd
 import numpy as np
 from scipy.signal import savgol_filter
 
-def savitzky_golay_filtering(timeseries, wnds=[7, 4], orders=[2, 3], debug=False):                                     
+def savitzky_golay_filtering(timeseries, wnds=[5, 4], orders=[2, 3], limit = 3, debug=False):                                     
 
-    ## Step 1: interopolate linearly between any missing points
-    interp_ts = timeseries.interpolate(method='linear', limit=14)
-    ## missing: doesn't go through and replace any dramatic jumps of 0.4 or more
+    ## Step 1: interopolate linearly between up to 3 missing points but not outside that 
+    interp_ts = timeseries.interpolate(method='linear', limit=limit, limit_area='inside')
+    final_ts = interp_ts.copy()
+    final_ts.iloc[:]=np.nan #create output series with right shape and nans in right places
+
+    ## missing: doesn't go through and replace any dramatic jumps of 0.4 or more. That's fine for my purposes.
+
+    ## Step 1.1 -  are there any holes still missing? If so,
+    # get the three longest portions of continuous data
+
+    # nans = interp_ts.isna()
+    # runs = nans.groupby(nans.cumsum()).agg({'index': ['count', 'min', 'max']})
+    # nans = interp_ts.isnull().astype(int).groupby(interp_ts.notnull().astype(int).cumsum()).cumsum()
+
+    # Extract out relevant column from dataframe as array
+    m = np.concatenate(( [True], np.isnan(interp_ts.values), [True] ))  # Mask
+    #kinda assuming there are no completely empty pixels 
+    #https://stackoverflow.com/questions/41494444/pandas-find-longest-stretch-without-nan-values
+
+    ss = np.flatnonzero(m[1:] != m[:-1]).reshape(-1,2)   # Start-stop limits
+    intervals, dum = np.shape(ss) #how many intervals are there 
+    #get the three longest by index
+    if intervals > 3: #if more than 3 pick the top 3
+        ss = ss[np.argpartition((ss[:,1] - ss[:,0]), -3)[-3:]]
+        intervals = 3
     
-    ## wnds = m, orders = d from paper 
-    ## Step 1.5?: selects the combo of m and d that has best fit
-    # according to the least squares (NOT IMPLEMENTED)                                                                                          
-    wnd, order = wnds[0], orders[0]
-    old_F = 1e8
-    new_F = 1e7 
-    W = []
-    iter = 0 
+    # start,stop = ss[(ss[:,1] - ss[:,0]).argmax()]  # Get max interval, interval limits
+    for x in range(0,intervals):
+        start, stop = ss[x, :] #get idx of the interval 
+        temp_ts = interp_ts.iloc[start:stop]
 
-    #STep 2: smooth with the SG filter to get the long term trend
-    # can use mode = 'wrap' -- check the notes and see if cyclic is good option
-    smoother_ts = savgol_filter(interp_ts, window_length=wnd, polyorder=order)                                     
-    diff = smoother_ts - interp_ts
-    sign = diff > 0 # any places where the smoothed trend is higher than og trend  
-    max_diff = np.max(np.abs(diff))                                                                                                                  
-
-    ## Step 3 Calculate the weights
-    #weight where the og trend is higher than the smoothed trned (diff < 0)
-    # are given higher weight
-    W = [(1 if (d<0)  else (1- abs(d)/max_diff)) for d in diff]
-    # print(W)                                                                                                                            
     
-    wnd, order = wnds[1], orders[1]
-    while (new_F < old_F) and (iter < 1000):
-        # Step 4: generate new series, replacing the things in 
-        # interp_ts with the things from the smoothed trend that are higher at 
-        # each point 
-        ##REPEAT FROM HERE
-        smooth_ts = smoother_ts * sign + interp_ts * (1 - sign)
-        '''
-        Ni(new) = interp_ts when interp > smoother, latest when interp < smoother
-        '''
-        # print('smoother: ', smoother_ts)
-        # print('interp: ', interp_ts)
-        # print('result (smooth): ', smooth_ts)
+    #apply the smoothing on each of these sections 
+        #put them all back together and return 
 
-        # Step 5: Fit new NDVI with SG filter
-        # set new m and d -  smaller m and larger d are recommended
-        # Chen 2004 used m=4 and d=6 
-        #except this savgol filter cannot hander an order higher than the window size 
-        smoother_ts = savgol_filter(smooth_ts, window_length=wnd, polyorder=order)
+
+        ## wnds = m, orders = d from paper 
+        ## Step 1.5?: selects the combo of m and d that has best fit
+        # according to the least squares (NOT IMPLEMENTED)                                                                                          
+        wnd, order = wnds[0], orders[0]
+        old_F = 1e8
+        new_F = 1e7 
+        W = []
+        iter = 0 
+
+        #STep 2: smooth with the SG filter to get the long term trend
+        # can use mode = 'wrap' -- check the notes and see if cyclic is good option
+        smoother_ts = savgol_filter(temp_ts, window_length=wnd, polyorder=order)                                     
+        diff = smoother_ts - temp_ts
+        sign = diff > 0 # any places where the smoothed trend is higher than og trend  
+        max_diff = np.max(np.abs(diff))                                                                                                                  
+
+        ## Step 3 Calculate the weights
+        #weight where the og trend is higher than the smoothed trned (diff < 0)
+        # are given higher weight
+        W = [(1 if (d<0)  else (1- abs(d)/max_diff)) for d in diff]
+        # print(W)                                                                                                                            
         
-        ## Step 6: Calculate fitting effect index 
-        # how well do fitted values approach the more highly weighted og points
-        diff = smoother_ts - interp_ts # Find the difference again                                
-        old_F = new_F
-        new_F = np.sum(np.abs(diff) * W)
-        iter +=1
-        sign = diff > 0 ## have to update the sign st that new trend is filled right 
-                                                                                          
-        # print(iter, ' : ', new_F)
-        #then repeat again unless the new F score is bigger than the last one
-        # indicating a worse fit, in which case values are not replaced and exits
+        wnd, order = wnds[1], orders[1]
+        while (new_F < old_F) and (iter < 10):
+            # Step 4: generate new series, replacing the things in 
+            # interp_ts with the things from the smoothed trend that are higher at 
+            # each point 
+            ##REPEAT FROM HERE
+            smooth_ts = smoother_ts * sign + temp_ts * (1 - sign)
+            '''
+            Ni(new) = interp_ts when interp > smoother, latest when interp < smoother
+            '''
+            # print('smoother: ', smoother_ts)
+            # print('interp: ', interp_ts)
+            # print('result (smooth): ', smooth_ts)
 
+            # Step 5: Fit new NDVI with SG filter
+            # set new m and d -  smaller m and larger d are recommended
+            # Chen 2004 used m=4 and d=6 
+            #except this savgol filter cannot hander an order higher than the window size 
+            smoother_ts = savgol_filter(smooth_ts, window_length=wnd, polyorder=order)
+            
+            ## Step 6: Calculate fitting effect index 
+            # how well do fitted values approach the more highly weighted og points
+            diff = smoother_ts - temp_ts # Find the difference again                                
+            old_F = new_F
+            new_F = np.sum(np.abs(diff) * W)
+            iter +=1
+            sign = diff > 0 ## have to update the sign st that new trend is filled right 
+                                                                                            
+            # print(iter, ' : ', new_F)
+            #then repeat again unless the new F score is bigger than the last one
+            # indicating a worse fit, in which case values are not replaced and exits
+        
+        #assign the smoothed section to the output series then repeat for other intervals
+        final_ts.iloc[start:stop] = smooth_ts
 
     if debug:
-        return smooth_ts, interp_ts
-    return smooth_ts
+        return final_ts, interp_ts
+    return final_ts
