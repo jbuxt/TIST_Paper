ec81cf320bcaf2ac651c012bba453cd16afb2aa5
m-henderson <maddiehenderson12@gmail.com>
2023-07-11 11:50:58 +0100

    calculating rates all valid rows one time period

--- calc_return_rate.py
+++ calc_return_rate.py
@@ -55,6 +55,7 @@ recovery_idx_Th = np.array( [59, 77, 116] )#Apr 18, Oct 19, Jan 23 (ignored  104
 # add for other counties 
 
 recov_start = recovery_idx_Th-7 #look 7 months back for the start of the recovery period
+## CHECK: SHOULD I GO BACK EVEN FARHTER? 
 recov_stop = recovery_idx_Th+4 #look up to 4 months to account for delay in veg greening 
 recov_stop[recov_stop > 119] = 119 #in case there's any that are close to the end 
 
@@ -67,80 +68,95 @@ ss_overlap = ss_overlap.astype('bool')
 ####### Calculate the recovery rates for each designated recovery period #####
 #Make this a loop that goes through the recov_start/stop list 
 
-valid_rows = ss_rows[ss_overlap[:,0]]#these are the rows to look at for the first recovery period 
-
+valid_rows = ss_rows[ss_overlap[:,1]]#these are the rows to look at for the first recovery period 
+n_valid_rows = len(valid_rows)
 # Grab the slices of the ndvi that we care about 
 valid_slices = ndvi_res.iloc[valid_rows, recov_start[0]+4:recov_stop[0]+4] # +4 because there are 4 extra columns at the front end of the df 
 
-########### Calc recovery rate sample ##############3
-#pix is a pd series
-sample = pix.astype("float32") #get relevant part 
-#-- now need to tell it where to look 
-#need to probably include clause that if the min isn't more than X% diff, then 
-#there was nothing to recover from 
+# Initialize an array to hold the resulting recovery rates and Rsq and fitted eqs for graphing 
+recov_rates = np.empty(n_valid_rows)
+recov_rates[:] = np.nan
+Rsq = np.empty(n_valid_rows)
+Rsq[:] = np.nan
+fits = np.empty(np.shape(valid_slices))
+fits[:] = np.nan
 
-sample = sample[sample.values.argmin():(sample.values.argmin()+4)] #get 6 mo after the min 
-#change-- specify how long i'm looking for recovery? 
-x = range(sample.size) #make fake time data for this sample 
+a_guess, b_guess, c_guess = -0.1, -0.3, 0.05 #Might need to edit this
 
-#potentially need to edit this 
-a_guess, b_guess, c_guess = 1, -0.01, 1
+for n in range(n_valid_rows): 
+    row = valid_slices.iloc[n, :]
+    local_min = row.values.argmin()
+    sample = row[local_min:] #Find the min within this time and start there 
+    x = range(sample.size) #make fake time data for this sample 
+    try:
+        popt, pcov = curve_fit(lambda t, a, b, c: a*np.exp(b*t)+c, x, sample, 
+                        p0=(a_guess, b_guess, c_guess))
+    # note that have to remove any values of sample that are zero along with corresponding x -- see if that's an issue 
 
-popt, pcov = curve_fit(lambda t, a, b, c: a*np.exp(b*t)+c, x, sample, 
-                       p0=(a_guess, b_guess, c_guess))
+        a = popt[0]
+        r = popt[1] #recovery rate
+        c = popt[2]
+        #for plotting and finding residual
+        y_curvefitted = a*np.exp(r*x)+c 
+        # curve_series = pd.Series(y_curvefitted, index=sample.index) #not sure this is right 
 
-# note that have to remove any values of sample that are zero along with corresponding x -- see if that's an issue 
-a = popt[0]
-r = popt[1] #recovery rate
-c = popt[2]
-#for plotting and finding residual (do i need to find residual?? )
-y_curvefitted = a*np.exp(r*x)+c 
-curve_series = pd.Series(y_curvefitted, index=sample.index)
+        #Calculate the residual between the fitted curve and data 
+        ss_res = np.nansum((sample - y_curvefitted)**2)
+        ss_tot = np.nansum((sample - sample.mean())**2)
+        rsquared = 1 - (ss_res/ss_tot)
+    except Exception as e: 
+        print(e)
+        # if you can't calculate for some reason 
+        r= -1 
+        rsquared = -1
+        y_curvefitted = np.nan
 
-#Calculate the residual between the fitted curve and data 
-ss_res = np.nansum((sample - y_curvefitted)**2)
-ss_tot = np.nansum((sample - sample.mean())**2)
-R_sq = 1 - (ss_res/ss_tot)
+    print('recovery rate: ', r, '\nRsq: ', rsquared)
 
-print('recovery rate: ', r, '\nRsq: ', R_sq)
+    recov_rates[n] = r
+    Rsq[n] = rsquared
+    fits[n, local_min:] = y_curvefitted
+
 
 
 ##########Plot ############################
-fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, sharey=False)
-# ax2 = host.twinx()
-ax1.set_xlabel('month')
-ax1.set_ylabel('monthly precip mm/mo' )
-# ax1.set_ylim(0, 600)
-ax1.bar(x=precip.index, height=precip, width=20, label='monthly precip mm')
+    # fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, sharey=False)
+    ax = plt.plot(row, 'r*', label='ndvi res')
+    # ax2 = host.twinx()
+    # ax1.set_xlabel('month')
+    # ax1.set_ylabel('monthly precip mm/mo' )
+    # # ax1.set_ylim(0, 600)
+    # ax1.bar(x=precip.index, height=precip, width=20, label='monthly precip mm')
 
 
-ax2.set_ylabel('NDVI residual')
-# ax2.set_ylim(-0.25, 0.25)
-ax2.plot(pix, 'r*', label='ndvi res')
-rec = 'fitted recovery, r='+str(round(r, 3))+' Rsq='+str(round(R_sq, 2))
-ax2.plot(curve_series, 'g-', label=rec)
-# fig.legend()
-ax1.grid(axis='x', which='major')
-ax2.grid(axis='x', which='major')
-ax2.legend()
-ax1.pcolor(date_plus, ax1.get_ylim(), 
-           NDMA['Tharaka'].map(color_dict).values[np.newaxis], 
-           cmap=CMAP, alpha=0.4,
-           linewidth=0, antialiased = True)
-#lol don't know how to do this to both at once
-ax2.pcolor(date_plus, ax2.get_ylim(), 
-           NDMA['Tharaka'].map(color_dict).values[np.newaxis], 
-           cmap=CMAP, alpha=0.4,
-           linewidth=0, antialiased = True)
+    # ax.set_ylabel('NDVI residual')
+    plt.ylabel('ndvi_residual')
+    # ax2.set_ylim(-0.25, 0.25)
+    rec = 'fitted recovery, r='+str(round(r, 3))+' Rsq='+str(round(rsquared, 2))
+    plt.plot(fits[n], 'g-', label=rec)
+    # fig.legend()
+    # ax.grid(axis='x', which='major')
+    # ax.grid(axis='x', which='major')
+    plt.legend()
+    # ax1.pcolor(date_plus, ax1.get_ylim(), 
+    #         NDMA['Tharaka'].map(color_dict).values[np.newaxis], 
+    #         cmap=CMAP, alpha=0.4,
+    #         linewidth=0, antialiased = True)
+    #lol don't know how to do this to both at once
+    # ax2.pcolor(date_plus, ax2.get_ylim(), 
+    #         NDMA['Tharaka'].map(color_dict).values[np.newaxis], 
+    #         cmap=CMAP, alpha=0.4,
+    #         linewidth=0, antialiased = True)
 
-fig.autofmt_xdate(rotation=45)
-#fix this -- set the
-cbar=fig.colorbar(cm.ScalarMappable(cmap=CMAP), ax=ax1, 
-                  location='top', shrink = 0.5,
-                  pad=0.2,
-                  label='NDMA Drought Status')
-cbar.set_ticks([0.12, 0.35, 0.62, 0.87])
-cbar.set_ticklabels(['Alarm', 'Alert', 'Recovery', 'Normal'])
-plt.show()
+    # plt.autofmt_xdate(rotation=45)
+    #fix this -- set the
+    # cbar=fig.colorbar(cm.ScalarMappable(cmap=CMAP), ax=ax1, 
+    #                 location='top', shrink = 0.5,
+    #                 pad=0.2,
+    #                 label='NDMA Drought Status')
+    # cbar.set_ticks([0.12, 0.35, 0.62, 0.87])
+    # cbar.set_ticklabels(['Alarm', 'Alert', 'Recovery', 'Normal'])
+    plt.show()
+    pass
 
 print('done')
