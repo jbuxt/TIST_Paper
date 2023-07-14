83f8ecb031ae9b6f6238811626bc3fc227cf02bc
m-henderson <maddiehenderson12@gmail.com>
2023-07-14 10:13:28 +0100

    hardcode county for remote start

--- deseason_detrend.py
+++ deseason_detrend.py
@@ -14,7 +14,8 @@ import pickle
 
 
 # Load the veg data #############################################
-county = input('Input the county to process: ')
+# county = input('Input the county to process: ')
+county = 'Tharaka'
 
 ndvi_df = pd.read_csv('ndvi_pixels_'+county+'.csv') #TEMP for testing 
 
