d411150597a7ed836099653b9d17d1376f20b55a
m-henderson <maddiehenderson12@gmail.com>
2023-08-08 16:54:32 +0100

    correlation and heatmap

--- stats.py
+++ stats.py
@@ -6,11 +6,12 @@ import matplotlib.cm as cm
 from matplotlib.colors import ListedColormap
 import pandas as pd
 import numpy as np 
+import seaborn as sb
 
 ######################################################33
 # import
 county =  'Tharaka'
-res = pd.read_csv('./RESULTS/V2/ndvi_results_w_everything'+county+'.csv')
+res = pd.read_csv('./RESULTS/V2/ndvi_results_w_everything'+county+'.csv', nrows = 1000)
 
 '''row,col,tist,county,mean,stdev,recov_rate_59,recov_rate_77,
 rsq_59,rsq_77,mins_59,mins_77,
@@ -19,6 +20,14 @@ landcover,eco,mean_ndvi,stdev_ndvi,alt,tist_neighbors'''
 recov_nums = [x[-2:] for x in res.filter(like='recov').columns.to_list()] #strings
 n_recovs = len(recov_nums)
 
+# make a bool col for calc
+for x in recov_nums:
+    col_name = 'recov_rate_'+x
+    new_col_name = 'calc_'+x
+    res[new_col_name] = (res[col_name] < 10) #10 is no calc and 15 is no disturbance
+
+cols_to_corr = ['mean_ndvi','stdev_ndvi','alt','mean','stdev']
+
 ##################################################################3333
 # Spearmans rank correlation 
 # Do for recovery rates (only ones that have calculated)
@@ -26,3 +35,23 @@ n_recovs = len(recov_nums)
 
 # can i also do it for landcover/ecoregion? how? 
 # can i also do it for categorical calc/no calc/no disturbance? 
+# do i want this separated by county? i think yes that's fine 
+
+# f, axs = plt.subplots(1, n_recovs, gridspec_kw=dict(width_ratios=[4, 4]))
+
+for x in recov_nums: 
+    col_name = 'recov_rate_'+x
+    bool_col_name = 'calc_'+x
+    spearmans = res.loc[res[bool_col_name], [col_name]+cols_to_corr].corr(method = 'spearman')
+    print(spearmans)
+    print(x)
+    plt.figure()
+    sb.heatmap(spearmans, vmin=-1.0, vmax=1.0, cmap='PRGn', annot=True, 
+               fmt=".2f")
+    plt.xticks(rotation=45) 
+    plt.yticks(rotation=45) 
+    plt.title('Spearmans Correlation for Recovery '+x+' in '+county)
+    plt.tight_layout()
+    plt.show()
+
+print('donezo')
