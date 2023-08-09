e7d3ed2ef9d32c97d3af270bb107a95d36c3325c
m-henderson <maddiehenderson12@gmail.com>
2023-08-09 12:29:25 +0100

    add precip to results files

--- yearly_avg_precip.py
+++ yearly_avg_precip.py
@@ -7,23 +7,27 @@ import numpy as np
 
 #################################################
 county = input('Input the county to process: ')
-pix_file = 'precip_pixels_'+county+'.csv'
+res_file = './RESULTS/V2/ndvi_results_w_everything'+county+'.csv'
+precip_file = './yearly_precip/yearly_precip'+county+'.csv'
 
-pix= pd.read_csv(pix_file)
-'''row, col, county, tist, 120 cols of dates'''
+# pix_file = 'precip_pixels_'+county+'.csv'
 
-yearly = pix.iloc[:, 4:].sum(axis = 1).astype('float16') / 10  #sum the monthly rainfall then divide by number of years 
+# pix= pd.read_csv(pix_file)
+# '''row, col, county, tist, 120 cols of dates'''
 
-precip = pd.concat([pix.loc[:, ['row', 'col']], yearly.rename('yearly_precip')], axis = 1)
+# yearly = pix.iloc[:, 4:].sum(axis = 1).astype('float16') / 10  #sum the monthly rainfall then divide by number of years 
 
-#add to results 
-# result_df = pd.read_csv(infile)
-# result_df = result_df.merge(ndvi_mean, how='left', on=['row', 'col'])
+# precip = pd.concat([pix.loc[:, ['row', 'col']], yearly.rename('yearly_precip')], axis = 1)
+
+#add to results#########################3 
+result_df = pd.read_csv(res_file)
+precip = pd.read_csv(precip_file)
+result_df = result_df.merge(precip, how='left', on=['row', 'col'])
 
 # save as a tif for all pix in county
 # df_to_map(ndvi_mean, ['mean_ndvi', 'stdev_ndvi'], savefile = True, fname = 'ndvi_mean_'+county+'.tif', meta = veg_meta)
 
 ################## save
-precip.to_csv('yearly_precip'+county+'.csv', encoding='utf-8', index=False)
-
+# precip.to_csv('yearly_precip'+county+'.csv', encoding='utf-8', index=False)
+result_df.to_csv('./RESULTS/V2/ndvi_all_results_'+county+'.csv', encoding='utf-8', index=False)
 print('added yearly precip for '+county)                   
