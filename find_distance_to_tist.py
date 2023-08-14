5c9d9ba7e554ff9865dd2a678a25dcdfe0aaad75
m-henderson <maddiehenderson12@gmail.com>
2023-08-14 17:57:18 +0100

    euclidean dist to csv

--- find_distance_to_tist.py
+++ find_distance_to_tist.py
@@ -10,18 +10,8 @@ import rasterio as rs
 import numpy as np
 import pickle
 from rasterio import features
-import cv2
 from df_to_map import df_to_map
 from scipy.ndimage import distance_transform_edt as edt
-
-#################################################
-# county = input('Input the county to process: ')
-
-# infile = './RESULTS/V2/ndvi_all_results_'+county+'.csv'
-# result_df = pd.read_csv(infile)
-
-# with open('veg_meta.pkl', 'rb') as file:
-#     veg_meta, veg_bounds = pickle.load(file)
 
 
 # get neighboring tist #######################
@@ -30,21 +20,40 @@ with open('TIST_mask.pkl', 'rb') as file:
     tist_mask = pickle.load(file)
 
 
-# save for maps ##############################
-# tist_meta = veg_meta
-# tist_meta['dtype'] = 'int32'
-# tist_meta['count'] = 1
+## EDT in scipy works by finding distance to closest 'background' pixel (zeros
+# so need to invert image first 
+dists = edt(np.logical_not(tist_mask))
+#calculates euclidean distance to closest 0 pixel
+# note that this is in distance of pixels
+#convert to meters 
+dists = dists * 30 
+# plt.imshow(dists)
+# plt.show()
 
-# with rs.open('tist_neighbors.tif',"w", **tist_meta) as f:
-#     #python array is row, cols, depth
-#     # rasterio expects band, row, col
-#     #can do a 2d with indexes=1
-#     f.write(tist_mask, indexes=1)
-#     # set layer descriptions 
+# save for maps ##############################
+with open('veg_meta.pkl', 'rb') as file:
+    veg_meta, veg_bounds = pickle.load(file)
+
+tist_meta = veg_meta
+tist_meta['dtype'] = 'float32'
+tist_meta['count'] = 1
+
+with rs.open('tist_distance.tif',"w", **tist_meta) as f:
+    #python array is row, cols, depth
+    # rasterio expects band, row, col
+    #can do a 2d with indexes=1
+    f.write(dists, indexes=1)
+    # set layer descriptions 
 
 # SAVE To result csvs############################# 
-# result_df['tist_neighbors'] = tist_mask[result_df['row'], result_df['col']]
 
-# result_df.to_csv(infile, encoding='utf-8', index=False)
+for county in ['Tharaka', 'Meru', 'Nyeri', 'Meru']:
 
-print('added mean ndvi, alt, pct missing, human modification, and tist neighbors for '+county)                   
+    infile = './RESULTS/V2/ndvi_all_results_'+county+'.csv'
+    result_df = pd.read_csv(infile)
+    
+    result_df['tist_distance'] = dists[result_df['row'], result_df['col']]
+
+    result_df.to_csv(infile, encoding='utf-8', index=False)
+
+print('added distance to TIST grove in m')                   
