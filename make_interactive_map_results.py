920ad999505cd3e0b6b589027f9b15e8f32a25b3
m-henderson <maddiehenderson12@gmail.com>
2023-09-22 16:42:17 -0500

    map of tharaka results

--- make_interactive_map_results.py
+++ make_interactive_map_results.py
@@ -7,6 +7,13 @@
 # add colorbar that's not ugly
 # popup color https://stackoverflow.com/questions/62789558/is-it-possible-to-change-the-popup-background-colour-in-folium
 
+
+for final results page want 
+- can use existing images for some parts of page - examples, all results 
+- for map for use -  could limit to meru and tharaka for speed/size
+--- for colormap, do separate layers for valid calc, invalid calc 
+- have 59, 77 as toggle layers, background google satellite, sample of TIST groves, and several
+  points with example plots that can be pulled up 
 '''
 # import pandas as pd 
 import folium as f
@@ -22,78 +29,69 @@ import pickle
 ###############################
 #STYLE 
 
-Viridis = mpl.colormaps['viridis']
-temp = Viridis(np.linspace(0, 1, 256))
-temp[:, 3] = 0.7 #set everything kinda transparent
+Green = mpl.colormaps['Greens']
+temp = Green(np.linspace(-1, 5, 256))
+# temp[:, 3] = 0.7 #set everything kinda transparent
 temp[0, :] = np.array([1,1,1,0]) #Set anything mapped to the min as transparent
-r_cm  = ListedColormap(temp)
+green_cm  = ListedColormap(temp)
 
-Purples = mpl.colormaps['Purples'] 
-temp1 = Purples(np.linspace(0,1,256))
-temp1[0, :] = np.array([1,1,1,0]) #Set anything mapped to 0 as transparent
-missing_cm = ListedColormap(temp1)
+# Purples = mpl.colormaps['Purples'] 
+# temp1 = Purples(np.linspace(0,1,256))
+# temp1[0, :] = np.array([1,1,1,0]) #Set anything mapped to 0 as transparent
+# missing_cm = ListedColormap(temp1)
 
 
-YlOrRd = mpl.colormaps['YlOrRd'] 
-temp2 = YlOrRd(np.linspace(0,1,256))
-temp2[0, :] = np.array([1,1,1,0]) #Set anything mapped to 0 as transparent
-missing_pct_cm = ListedColormap(temp2) 
+# YlOrRd = mpl.colormaps['YlOrRd'] 
+# temp2 = YlOrRd(np.linspace(0,1,256))
+# temp2[0, :] = np.array([1,1,1,0]) #Set anything mapped to 0 as transparent
+# missing_pct_cm = ListedColormap(temp2) 
 
-#for the problematic ones 
-cmap = plt.get_cmap('PuOr', 2)
-cmap.set_under('#FFFFFF00')
+# #for the problematic ones 
+# cmap = plt.get_cmap('PuOr', 2)
+# cmap.set_under('#FFFFFF00')
 
 
 ######################## # ##################################
 ##Make the pngs that get loaded later (only need to do once)
 
-# with open('county_mask.pkl', 'rb') as file:
-#    county_mask= pickle.load(file)
-# new = county_mask != 0 
-# county_mask = new * 1
-
-
-with rs.open('RESULTS/V2/ndvi_results_Meru_V2.tif') as im:
-  meru = im.read() #outside of county is nan
+with rs.open('77_Tharaka.tif') as im:
+  thar77 = im.read() #outside of county is nan
   meru_bands = im.descriptions
-np.nan_to_num(meru, copy=False, nan=-100)
-with rs.open('RESULTS/V2/ndvi_results_Tharaka_V2.tif') as im:
-  thar = im.read() 
-  thar_bands =im.descriptions
-np.nan_to_num(thar, copy=False, nan=-100)
-with rs.open('RESULTS/V2/ndvi_results_Nyeri_V2.tif') as im:
-  nyeri = im.read() #
-  nyeri_bands =im.descriptions
-np.nan_to_num(nyeri, copy=False, nan=-100)
 
-with rs.open('RESULTS/V2/ndvi_results_Embu_V2.tif') as im:
-  embu = im.read() #outside of county is nan
-  embu_bands = im.descriptions
-np.nan_to_num(embu, copy=False, nan=-100)
+with rs.open('59_Tharaka.tif') as im:
+  thar59 = im.read() 
+  thar_bands =im.descriptions
 
 #Goals 
 # 2 images for each recovery - one of the legit vals , one of the 10s and 15s 
 
-# # # let zeros be the nodata value that i will make clear 
-# # could set all nodata to -1 for these which would get mapped as clear if i moved the range up by one 
-# missing_streak = meru[1,:,:] +  thar[1,:,:] +  nyeri[1,:,:] + embu[1,:,:] + lai[1,:,:] 
-# missing_months = meru[0,:,:] +  thar[0,:,:] +  nyeri[0,:,:] + embu[0,:,:] + lai[0,:,:]
-# #should probably also save this in one image / csv for spotfire analysis 
+recov77 = thar77[0,:,:] 
+np.nan_to_num(recov77, copy=False, nan=1) #rates are negative 
+others77 = recov77.copy() #for 10s and 15s
+others77[others77 < 10] =  0 #get rid of all others
+recov77[recov77 >= 10] =  1
+recov77 = recov77 * -1 #make rates positive and NAN/10/15 is negative -1
+recov77[recov77 >= 5] =  5 #make everything more than 5 just 5 for the picture
 
-# #change nan to 0 for the picture 
-# # np.nan_to_num(missing_streak, copy=False, nan=0.0)
-# # np.nan_to_num(missing_months, copy=False, nan=0.0)
+recov59 = thar59[0,:,:] 
+np.nan_to_num(recov59, copy=False, nan=1) #rates are negative 
+others59 = recov59.copy() #for 10s and 15s
+others59[others59 < 10] =  0 #get rid of all others
+recov59[recov59 >= 10] =  1
+recov59 = recov59 * -1 #make rates positive and NAN/10/15 is negative -1
+recov59[recov59 >= 5] =  5 #make everything more than 5 just 5 for the picture
 
-# # # #normalize and colorize
-# missing_months_colors = missing_pct_cm( np.round(((missing_months - 0) * (1/(1 - 0) * 255)), 0).astype('int16')) #I know the max missing months % is 1
-# missing_streak_colors = missing_cm( np.round(((missing_streak - 0) * (1/(36 - 0) * 255)), 0).astype('int16')) # I know the max missing streak is like 50
-# # # #Save as png 
-# mpl.image.imsave('missing_mo_for_map.png', missing_months_colors)
-# mpl.image.imsave('missing_streak_for_map.png', missing_streak_colors)
+# # #normalize and colorize
+recov77_colors = green_cm( np.round(((recov77 - -1) * (1/(5 - -1) * 255)), 0).astype('int16')) #min -1 max 5 
+recov59_colors = green_cm( np.round(((recov59 - -1) * (1/(5 - -1) * 255)), 0).astype('int16')) #min -1 max 5 
+
+# # #Save as png 
+mpl.image.imsave('77_thar.png', recov77)
+mpl.image.imsave('59_thar.png', recov59)
 
 ###################################################################3
 # Define the center of map
-lon, lat = 37.61, 0.18 
+lon, lat = 37.61, 0.18  #EDIT THIS TO BE ON THARAKA
 my_map = f.Map(location=[lat, lon], zoom_start=8)
 
 #for all the bounding boxes of images in the ROI
@@ -105,9 +103,9 @@ t_bounds = [[-0.9639821313886587, 36.11092694867859], [0.8877151637669112, 38.47
 with open('tist_and_counties_gp.pkl', 'rb') as file:
    tist_gp, counties_gp= pickle.load(file)
 
-# select = np.round(np.random.rand(5000) * 30000, 0).astype('int16')# get 5000 random tist groves
-##reduced for putting on web
-# tist_gp = tist_gp.loc[select, ['Trees', 'Name', 'geometry']]
+select = np.round(np.random.rand(5000) * 30000, 0).astype('int16')# get 5000 random tist groves
+#reduced for putting on web
+tist_gp = tist_gp.loc[select, ['Trees', 'Name', 'geometry']]
 
 
 f.GeoJson(tist_gp, name='TIST Groves',
@@ -120,44 +118,48 @@ f.GeoJson(tist_gp, name='TIST Groves',
 
 f.raster_layers.ImageOverlay('missing_streak_for_map.png', #LONGEST MISSING STREAK 
                   bounds=t_bounds, #[[lat_min, lon_min], [lat_max, lon_max]]
-                  name = 'Longest missing streak (months) in Landsat data',
+                  name = 'Recovery rate for Recovery 59',
                   ).add_to(my_map)
 
 
 f.raster_layers.ImageOverlay('missing_mo_for_map.png',
                   bounds=t_bounds, #[[lat_min, lon_min], [lat_max, lon_max]
-                  name = '% Months missing from Landsat data',
+                  name = 'Recovery rate for Recovery 77',
                   ).add_to(my_map)
 
 
 ###############################################################
 # FINISH AND SAVE
 #colormaps since folium doesn't seem to play well with matplotlib
-cm1 = f.LinearColormap(colors = [tuple(row) for row in temp2], 
-                       index = np.linspace(0, 1, num=255),
-                       vmin = 0, 
-                       vmax = 1, 
-                       caption='Percent of months missing from Landsat-8 data (2013-23)') #probably need to edit this to correspond max/min of usecase
-cm2 = f.LinearColormap(colors = [tuple(row) for row in temp1],
-                       index = np.linspace(0, 36, num=255),
-                       vmin = 0, 
-                       vmax = 36, 
-                       caption='Number of missing months in longest missing streak')
+# cm1 = f.LinearColormap(colors = [tuple(row) for row in temp2], 
+#                        index = np.linspace(0, 1, num=255),
+#                        vmin = 0, 
+#                        vmax = 1, 
+#                        caption='Percent of months missing from Landsat-8 data (2013-23)') #probably need to edit this to correspond max/min of usecase
+# cm2 = f.LinearColormap(colors = [tuple(row) for row in temp1],
+#                        index = np.linspace(0, 36, num=255),
+#                        vmin = 0, 
+#                        vmax = 36, 
+#                        caption='Number of missing months in longest missing streak')
 # cm3 = f.LinearColormap(colors = [tuple(row) for row in temp3],
 #                        index = np.linspace(-1, 1, num=255),
 #                        vmin = -1, 
 #                        vmax = 1, #IDIOT 
 #                        caption='NDVI (Vegetation Greeness)')
-
+cm1 = f.LinearColormap(colors = [tuple(row) for row in temp], 
+                       index = np.linspace(0, 1, num=255),
+                       vmin = 0, 
+                       vmax = 1, 
+                       caption='Percent of months missing from Landsat-8 data (2013-23)') #probably need to edit this to correspond max/min of usecase
 #add a background to the colorbar - hacky and stolen from
 #https://stackoverflow.com/questions/44887461/how-to-add-a-background-color-of-a-colormap-in-a-folium-map
 cmap_HTML = cm1._repr_html_()
 cmap_HTML = cmap_HTML.replace('<svg height="50" width="400">','<svg id="cmap" height="50" width="400">',1)
 cmap_style = '<style>#cmap {background-color: green;}</style>'
 
-cmap2_HTML = cm2._repr_html_()
-cmap2_HTML = cmap2_HTML.replace('<svg height="50" width="400">','<svg id="cmap" height="50" width="400">',1)
-cmap2_style = '<style>#cmap {background-color: green;}</style>'
+# cmap2_HTML = cm2._repr_html_()
+# cmap2_HTML = cmap2_HTML.replace('<svg height="50" width="400">','<svg id="cmap" height="50" width="400">',1)
+# cmap2_style = '<style>#cmap {background-color: green;}</style>'
 
 # cmap3_HTML = cm3._repr_html_()
 # cmap3_HTML = cmap3_HTML.replace('<svg height="50" width="400">','<svg id="cmap" height="50" width="400">',1)
