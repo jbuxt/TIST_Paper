a5eb1a46d8d1af66ecaec38a8c9736848cbbd713
m-henderson <maddiehenderson12@gmail.com>
2023-09-22 17:57:33 -0500

    added Esri satellite tile layer

--- make_interactive_map_results.py
+++ make_interactive_map_results.py
@@ -50,49 +50,58 @@ other_cm = LinearSegmentedColormap.from_list( 'other_cm', [(0/15,    '#ffff0000'
 ######################## # ##################################
 ##Make the pngs that get loaded later (only need to do once)
 
-with rs.open('77_Tharaka.tif') as im:
-  thar77 = im.read() #outside of county is nan
-  meru_bands = im.descriptions
+# with rs.open('77_Tharaka.tif') as im:
+#   thar77 = im.read() #outside of county is nan
+#   meru_bands = im.descriptions
 
-with rs.open('59_Tharaka.tif') as im:
-  thar59 = im.read() 
-  thar_bands =im.descriptions
+# with rs.open('59_Tharaka.tif') as im:
+#   thar59 = im.read() 
+#   thar_bands =im.descriptions
 
-#Goals 
-# 2 images for each recovery - one of the legit vals , one of the 10s and 15s 
+# #Goals 
+# # 2 images for each recovery - one of the legit vals , one of the 10s and 15s 
 
-recov77 = thar77[0,:,:] 
-np.nan_to_num(recov77, copy=False, nan=1) #rates are negative 
-others77 = recov77.copy() #for 10s and 15s
-others77[others77 < 10] =  0 #get rid of all others
-recov77[recov77 >= 10] =  1
-recov77 = recov77 * -1 #make rates positive and NAN/10/15 is negative -1
-recov77[recov77 >= 5] =  5 #make everything more than 5 just 5 for the picture
+# recov77 = thar77[0,:,:] 
+# np.nan_to_num(recov77, copy=False, nan=1) #rates are negative 
+# others77 = recov77.copy() #for 10s and 15s
+# others77[others77 < 10] =  0 #get rid of all others
+# recov77[recov77 >= 10] =  1
+# recov77 = recov77 * -1 #make rates positive and NAN/10/15 is negative -1
+# recov77[recov77 >= 5] =  5 #make everything more than 5 just 5 for the picture
 
-recov59 = thar59[0,:,:] 
-np.nan_to_num(recov59, copy=False, nan=1) #rates are negative 
-others59 = recov59.copy() #for 10s and 15s
-others59[others59 < 10] =  0 #get rid of all others
-recov59[recov59 >= 10] =  1
-recov59 = recov59 * -1 #make rates positive and NAN/10/15 is negative -1
-recov59[recov59 >= 5] =  5 #make everything more than 5 just 5 for the picture
+# recov59 = thar59[0,:,:] 
+# np.nan_to_num(recov59, copy=False, nan=1) #rates are negative 
+# others59 = recov59.copy() #for 10s and 15s
+# others59[others59 < 10] =  0 #get rid of all others
+# recov59[recov59 >= 10] =  1
+# recov59 = recov59 * -1 #make rates positive and NAN/10/15 is negative -1
+# recov59[recov59 >= 5] =  5 #make everything more than 5 just 5 for the picture
 
-# # #normalize and colorize
-recov77_colors = green_cm( np.round(((recov77 - -1) * (1/(5 - -1) * 255)), 0).astype('int16')) #min -1 max 5 
-recov59_colors = green_cm( np.round(((recov59 - -1) * (1/(5 - -1) * 255)), 0).astype('int16')) #min -1 max 5 
+# # # #normalize and colorize
+# recov77_colors = green_cm( np.round(((recov77 - -1) * (1/(5 - -1) * 255)), 0).astype('int16')) #min -1 max 5 
+# recov59_colors = green_cm( np.round(((recov59 - -1) * (1/(5 - -1) * 255)), 0).astype('int16')) #min -1 max 5 
 
-other77_colors = other_cm(others77/15)
-other59_colors = other_cm(others59/15)
-# # #Save as png 
-mpl.image.imsave('77_thar.png', recov77_colors)
-mpl.image.imsave('59_thar.png', recov59_colors)
-mpl.image.imsave('77_other.png', other77_colors)
-mpl.image.imsave('59_other.png', other59_colors)
+# other77_colors = other_cm(others77/15)
+# other59_colors = other_cm(others59/15)
+# # # #Save as png 
+# mpl.image.imsave('77_thar.png', recov77_colors)
+# mpl.image.imsave('59_thar.png', recov59_colors)
+# mpl.image.imsave('77_other.png', other77_colors)
+# mpl.image.imsave('59_other.png', other59_colors)
 
 ###################################################################3
 # Define the center of map
-lon, lat = 37.61, 0.18  #EDIT THIS TO BE ON THARAKA
-my_map = f.Map(location=[lat, lon], zoom_start=8)
+lon, lat = 37.79, -0.24  #EDIT THIS TO BE ON THARAKA # 37.61, 0.18 for overall map
+
+my_map = f.Map(location=[lat, lon], zoom_start=9)
+
+tile = f.TileLayer(
+        tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        attr = 'Esri',
+        name = 'Esri Satellite',
+        overlay = False,
+        control = True
+       ).add_to(my_map)
 
 #for all the bounding boxes of images in the ROI
 t_bounds = [[-0.9639821313886587, 36.11092694867859], [0.8877151637669112, 38.47870637456082]]
@@ -105,7 +114,7 @@ with open('tist_and_counties_gp.pkl', 'rb') as file:
 
 # select = np.round(np.random.rand(5000) * 30000, 0).astype('int16')# get 5000 random tist groves
 #reduced for putting on web
-tist_gp = tist_gp.loc[tist_gp['COUNTY' == 'Tharaka'], ['Trees', 'Name', 'geometry']] #get only those in Tharaka 
+tist_gp = tist_gp.loc[tist_gp['COUNTY'] == 'Tharaka', ['Trees', 'Name', 'geometry']] #get only those in Tharaka 
 
 
 f.GeoJson(tist_gp, name='TIST Groves',
@@ -116,69 +125,60 @@ f.GeoJson(tist_gp, name='TIST Groves',
 ##################################################################################3
 # RASTER LAYERS
 
-f.raster_layers.ImageOverlay('missing_streak_for_map.png', #LONGEST MISSING STREAK 
+
+
+f.raster_layers.ImageOverlay('59_thar.png', 
                   bounds=t_bounds, #[[lat_min, lon_min], [lat_max, lon_max]]
                   name = 'Recovery rate for Recovery 59',
+                  overlay = True,
+                  control = True
                   ).add_to(my_map)
 
 
-f.raster_layers.ImageOverlay('missing_mo_for_map.png',
+f.raster_layers.ImageOverlay('77_thar.png',
                   bounds=t_bounds, #[[lat_min, lon_min], [lat_max, lon_max]
                   name = 'Recovery rate for Recovery 77',
+                  overlay = True,
+                  control = True
                   ).add_to(my_map)
 
+f.raster_layers.ImageOverlay('59_other.png', 
+                  bounds=t_bounds, #[[lat_min, lon_min], [lat_max, lon_max]]
+                  name = 'No disturbance / no calculation for Recovery 59',
+                  overlay = True,
+                  control = True                  
+                  ).add_to(my_map)
+
+
+f.raster_layers.ImageOverlay('77_other.png',
+                  bounds=t_bounds, #[[lat_min, lon_min], [lat_max, lon_max]
+                  name = 'No disturbance / no calculation for Recovery 77',
+                  overlay = True,
+                  control = True  
+                  ).add_to(my_map)
 
 ###############################################################
 # FINISH AND SAVE
-#colormaps since folium doesn't seem to play well with matplotlib
-# cm1 = f.LinearColormap(colors = [tuple(row) for row in temp2], 
-#                        index = np.linspace(0, 1, num=255),
-#                        vmin = 0, 
-#                        vmax = 1, 
-#                        caption='Percent of months missing from Landsat-8 data (2013-23)') #probably need to edit this to correspond max/min of usecase
-# cm2 = f.LinearColormap(colors = [tuple(row) for row in temp1],
-#                        index = np.linspace(0, 36, num=255),
-#                        vmin = 0, 
-#                        vmax = 36, 
-#                        caption='Number of missing months in longest missing streak')
-# cm3 = f.LinearColormap(colors = [tuple(row) for row in temp3],
-#                        index = np.linspace(-1, 1, num=255),
-#                        vmin = -1, 
-#                        vmax = 1, #IDIOT 
-#                        caption='NDVI (Vegetation Greeness)')
-cm1 = f.LinearColormap(colors = [tuple(row) for row in temp], 
-                       index = np.linspace(0, 1, num=255),
-                       vmin = 0, 
-                       vmax = 1, 
-                       caption='Percent of months missing from Landsat-8 data (2013-23)') #probably need to edit this to correspond max/min of usecase
+
+# add a colormap from a png since there's 2 sep pngs and the colormaps are so hacky 
+
 #add a background to the colorbar - hacky and stolen from
 #https://stackoverflow.com/questions/44887461/how-to-add-a-background-color-of-a-colormap-in-a-folium-map
-cmap_HTML = cm1._repr_html_()
-cmap_HTML = cmap_HTML.replace('<svg height="50" width="400">','<svg id="cmap" height="50" width="400">',1)
-cmap_style = '<style>#cmap {background-color: green;}</style>'
-
-# cmap2_HTML = cm2._repr_html_()
-# cmap2_HTML = cmap2_HTML.replace('<svg height="50" width="400">','<svg id="cmap" height="50" width="400">',1)
-# cmap2_style = '<style>#cmap {background-color: green;}</style>'
-
-# cmap3_HTML = cm3._repr_html_()
-# cmap3_HTML = cmap3_HTML.replace('<svg height="50" width="400">','<svg id="cmap" height="50" width="400">',1)
-# cmap3_style = '<style>#cmap {background-color: green;}</style>'
+# cmap_HTML = cm1._repr_html_()
+# cmap_HTML = cmap_HTML.replace('<svg height="50" width="400">','<svg id="cmap" height="50" width="400">',1)
+# cmap_style = '<style>#cmap {background-color: green;}</style>'
 
 
 
-my_map.get_root().header.add_child(f.Element(cmap_style))
-my_map.get_root().header.add_child(f.Element(cmap2_style))
-# my_map.get_root().header.add_child(f.Element(cmap3_style))
+
+# my_map.get_root().header.add_child(f.Element(cmap_style))
+
 
 f.map.LayerControl().add_to(my_map)
 
-my_map.get_root().html.add_child(f.Element(cmap_HTML))
-my_map.get_root().html.add_child(f.Element(cmap2_HTML))
-# my_map.get_root().html.add_child(f.Element(cmap3_HTML))
+# my_map.get_root().html.add_child(f.Element(cmap_HTML))
 
 
-
-my_map.save('results_map.html')
+my_map.save('tharaka_interactive_map.html')
 
 print('donezo')
