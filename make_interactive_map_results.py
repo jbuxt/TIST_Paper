0457d9b185e5c7a34e444e4264fcfd2e9b444840
m-henderson <maddiehenderson12@gmail.com>
2023-09-23 17:54:11 -0500

    added popup. popup image broken

--- make_interactive_map_results.py
+++ make_interactive_map_results.py
@@ -19,82 +19,13 @@ Did i already remove the roads etc from this ?? or are they so light? is this ex
 import folium as f
 from folium.features import GeoJsonPopup, GeoJsonTooltip
 from folium.plugins import FloatImage
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-import rasterio as rs
-import geopandas as gp
-import numpy as np
-from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+from folium import IFrame
 import pickle
+import base64
 
-###############################
-#STYLE 
-
-# Green = mpl.colormaps['Greens']
-# temp = Green(np.linspace(-1, 5, 256))
-# temp[0, :] = np.array([1,1,1,0]) #Set anything mapped to the min as transparent
-# green_cm  = ListedColormap(temp)
-
-# other_cm = LinearSegmentedColormap.from_list( 'other_cm', [(0/15,    '#ffff0000'), #first one is transparent
-#                                               (10/15, '#ffff66'),
-#                                               (15/15,    '#0000ff')])
-# YlOrRd = mpl.colormaps['YlOrRd'] 
-# temp2 = YlOrRd(np.linspace(0,1,256))
-# temp2[0, :] = np.array([1,1,1,0]) #Set anything mapped to 0 as transparent
-# missing_pct_cm = ListedColormap(temp2) 
-
-# #for the problematic ones 
-# cmap = plt.get_cmap('PuOr', 2)
-# cmap.set_under('#FFFFFF00')
-
-
-######################## # ##################################
-##Make the pngs that get loaded later (only need to do once)
-
-# with rs.open('77_Tharaka.tif') as im:
-#   thar77 = im.read() #outside of county is nan
-#   meru_bands = im.descriptions
-
-# with rs.open('59_Tharaka.tif') as im:
-#   thar59 = im.read() 
-#   thar_bands =im.descriptions
-
-# #Goals 
-# # 2 images for each recovery - one of the legit vals , one of the 10s and 15s 
-
-# recov77 = thar77[0,:,:] 
-# np.nan_to_num(recov77, copy=False, nan=1) #rates are negative 
-# others77 = recov77.copy() #for 10s and 15s
-# others77[others77 < 10] =  0 #get rid of all others
-# recov77[recov77 >= 10] =  1
-# recov77 = recov77 * -1 #make rates positive and NAN/10/15 is negative -1
-# recov77[recov77 >= 5] =  5 #make everything more than 5 just 5 for the picture
-
-# recov59 = thar59[0,:,:] 
-# np.nan_to_num(recov59, copy=False, nan=1) #rates are negative 
-# others59 = recov59.copy() #for 10s and 15s
-# others59[others59 < 10] =  0 #get rid of all others
-# recov59[recov59 >= 10] =  1
-# recov59 = recov59 * -1 #make rates positive and NAN/10/15 is negative -1
-# recov59[recov59 >= 5] =  5 #make everything more than 5 just 5 for the picture
-
-# # # #normalize and colorize
-# recov77_colors = green_cm( np.round(((recov77 - -1) * (1/(5 - -1) * 255)), 0).astype('int16')) #min -1 max 5 
-# recov59_colors = green_cm( np.round(((recov59 - -1) * (1/(5 - -1) * 255)), 0).astype('int16')) #min -1 max 5 
-
-# other77_colors = other_cm(others77/15)
-# other59_colors = other_cm(others59/15)
-# # # #Save as png 
-# mpl.image.imsave('77_thar.png', recov77_colors)
-# mpl.image.imsave('59_thar.png', recov59_colors)
-# mpl.image.imsave('77_other.png', other77_colors)
-# mpl.image.imsave('59_other.png', other59_colors)
-
-###################################################################3
+###################################################3
 # Define the center of map
 lon, lat = 37.79, -0.24  #EDIT THIS TO BE ON THARAKA # 37.61, 0.18 for overall map
-
-my_map = f.Map(location=[lat, lon], zoom_start=10)
 
 tile = f.TileLayer(
         tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
@@ -102,8 +33,10 @@ tile = f.TileLayer(
         name = 'Esri Satellite',
         overlay = False,
         control = True, 
-        show = True
-       ).add_to(my_map)
+        show = True, 
+       )
+
+my_map = f.Map(location=[lat, lon], zoom_start=10, tiles = tile)
 
 #for all the bounding boxes of images in the ROI
 # t_bounds = [[-0.9639821313886587, 36.11092694867859], [0.8877151637669112, 38.47870637456082]] #original ROI
@@ -128,6 +61,13 @@ f.GeoJson(tist_gp, name='TIST Groves',
 ##################################################################################3
 # RASTER LAYERS
 
+f.raster_layers.ImageOverlay('./external_programs/thar_highres_59.png', 
+                  bounds=t_bounds, #[[lat_min, lon_min], [lat_max, lon_max]]
+                  name = 'Recovery rates 59 (Apr 2019)',
+                  overlay = True,
+                  control = True, 
+                  show = True, 
+                  ).add_to(my_map)
 
 f.raster_layers.ImageOverlay('./external_programs/thar_highres.png', 
                   bounds=t_bounds, #[[lat_min, lon_min], [lat_max, lon_max]]
@@ -137,40 +77,28 @@ f.raster_layers.ImageOverlay('./external_programs/thar_highres.png',
                   show = True, 
                   ).add_to(my_map)
 
+################################################################
+# EXAMPLE GRAPHS
+width = 608
+height = 348
+loc1 = [-0.310604, 37.807015]
+encoded = base64.b64encode(open('ex_graph1.png', 'rb').read())
+html = '<img src="data:image/png;base64,{}">'.format
+iframe = IFrame(html(encoded), width=(width)+20, height=(height)+20)
+popup = f.Popup(iframe, max_width=2650)
 
-# f.raster_layers.ImageOverlay('77_thar.png',
-#                   bounds=t_bounds, #[[lat_min, lon_min], [lat_max, lon_max]
-#                   name = 'Recovery rate for Recovery 77',
-#                   overlay = True,
-#                   control = True
-#                   ).add_to(my_map)
-
+icon = f.Icon(color="purple", icon = 'comment')
+marker = f.Marker(location=loc1, popup=popup, icon=icon)
+marker.add_to(my_map)
 
 
 ###############################################################
 # FINISH AND SAVE
 
-# add a colormap from a png since there's 2 sep pngs and the colormaps are so hacky 
+FloatImage('legend_smaller.png', bottom=1, left=1).add_to(my_map)
+#colormap legend since its kinda hard to make te he 
 
-#add a background to the colorbar - hacky and stolen from
-#https://stackoverflow.com/questions/44887461/how-to-add-a-background-color-of-a-colormap-in-a-folium-map
-# cmap_HTML = cm1._repr_html_()
-# cmap_HTML = cmap_HTML.replace('<svg height="50" width="400">','<svg id="cmap" height="50" width="400">',1)
-# cmap_style = '<style>#cmap {background-color: green;}</style>'
-
-
-
-
-
-# my_map.get_root().header.add_child(f.Element(cmap_style))
-
-FloatImage('legend_results_small.png', bottom=3, right=3).add_to(my_map)
 f.map.LayerControl().add_to(my_map)
-
-
-
-# my_map.get_root().html.add_child(f.Element(cmap_HTML))
-
 
 my_map.save('tharaka_interactive_map.html')
 
