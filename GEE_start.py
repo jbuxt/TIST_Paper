9ad2c0d153a67adbebe26ab7022de13711e47713
jbuxt <j.buxton@exeter.ac.uk>
2024-04-11 13:53:04 +0100

    Update login credentials

--- GEE_start.py
+++ GEE_start.py
@@ -4,7 +4,8 @@ import ee
 import google.auth
 # ee.Authenticate()
 # ee.Initialize()
-credentials, project_id = google.auth.default()
-ee.Initialize(credentials, project='ee-maddiehenderson12')
+#credentials, project_id = google.auth.default()
+ee.Initialize(project='ee-joshuabuxton2018')
+#ee.Initialize(credentials, project='ee-joshuabuxton2018')
 
 print(ee.Image("NASA/NASADEM_HGT/001").get("title").getInfo())
